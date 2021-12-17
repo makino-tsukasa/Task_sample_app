f16abb7d6896b08c7ebf35af00b02631c6e63f99
makinotsukasa <t.makino@qinc.work>
2021-12-17 20:12:57 +0900

    createアクションの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,33 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :logged_in_user, only: [:show, :index, :edit, :update, :destroy] 
+  before_action :logged_in_user, only: [:show, :index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:index]
   before_action :admin_or_correct_user, only: [:show]
   before_action :already_logged_in, only: [:new, :create]
-  
+
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
-    if @user.save
-      log_in @user
-      flash[:success] = '新規作成に成功しました。'
-      redirect_to @user
-    else
-      render :new
-    end
+    @user.save
+    log_in @user
+    flash[:success] = '新規作成に成功しました。'
+    redirect_to @usr
   end
-  
+
   def show
   end
-  
+
   def index
     @users = User.paginate(page: params[:page], per_page: 20)
   end
 
   def edit
   end
-  
+
   def update
     if @user.update_attributes(user_params)
       flash[:success] = "#{@user.name}の情報を更新しました。"
@@ -40,7 +37,7 @@ class UsersController < ApplicationController
       render :edit
     end
   end
-  
+
   def destroy
     if @user.destroy
       flash[:success] = "#{@user.name}を削除しました。"
@@ -50,14 +47,14 @@ class UsersController < ApplicationController
       render :index
     end
   end
-  
-  
+
+
   private
-  
+
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
-    
+
     def set_user
       @user = User.find(params[:id])
     end
